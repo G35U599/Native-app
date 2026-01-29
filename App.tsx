1ca7d240bc1f109bbd300512334d42ccf84c4f36
Jesus Enrique Fernandez Prieto <jesusefp99@gmail.com>
2026-01-28 21:47:39 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text >{counter}</Text>
+      <Pressable style={styles.button} onPress={() => setCounter(counter + 1)}>
+        <Text>Increment</Text>
+      </Pressable>
+      <Pressable style={styles.button} onPress={() => setCounter(counter - 1)}>
+        <Text>Decrement</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#ddd',
+    borderRadius: 5,
   },
 });
